163dc2956b9a74b6d1b944e0688799e9a2685c03
Reymark Torres <reymark.torres08.dev@gmail.com>
2020-09-22 22:50:33 +0800

    Add Retailer, Product and Stock relationships

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,33 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\Stock;
+use App\Models\Product;
+use App\Models\Retailer;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function it_checks_stock_for_products_at_retailers()
+    {
+        $switch = Product::create(['name' => 'Nintendo Switch']);
+
+        $bestBuy = Retailer::create(['name' => 'Best Buy']);
+
+        $this->assertFalse($switch->inStock());
+
+        $stock = new Stock([
+            'price' => '1000',
+            'url' => 'foo.com',
+            'sku' => '12345',
+            'in_stock' => true
+        ]);
+
+        $bestBuy->addStock($switch, $stock);
+        $this->assertTrue($switch->inStock());
     }
 }
